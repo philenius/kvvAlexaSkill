5622cdd4d9aac1f9249742693f541409ab96ad54
philenius <philippus-perez@web.de>
2017-12-05 00:15:20 +0100

    add welcome card

--- app/handlers/newSessionHandlers.js
+++ app/handlers/newSessionHandlers.js
@@ -3,7 +3,13 @@ const States = require('./states');
 module.exports = {
     'NewSession': function () {
         this.handler.state = States.SELECTSTOP;
-        this.emit(':ask', this.t('WELCOME'), this.t('WELCOME_REPROMPT'));
+        var cardTitle = 'Willkommen beim KVV';
+        var cardContent = 'VBK - Verkehrsbetriebe Karlsruhe';
+        var imageObj = {
+            smallImageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Tram_at_Karlsruhe_Marktplatz_-_geo.hlipp.de_-_4294.jpg',
+            largeImageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Tram_at_Karlsruhe_Marktplatz_-_geo.hlipp.de_-_4294.jpg'
+        };
+        this.emit(':askWithCard', this.t('WELCOME'), this.t('WELCOME_REPROMPT'), cardTitle, cardContent, imageObj);
     },
     'LaunchRequest': function () {
     },
