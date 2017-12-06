2ab0a3061188614bcf58f6e31f98a1655273cc40
philenius <philippus-perez@web.de>
2017-12-06 19:27:24 +0100

    add card to voice output

--- app/handlers/routeHandlers.js
+++ app/handlers/routeHandlers.js
@@ -1,17 +1,27 @@
 const Alexa = require('alexa-sdk');
 const States = require('./states');
+const util = require('../utils/util');
 
 module.exports = Alexa.CreateStateHandler(States.SELECTROUTE, {
     'NewSession': function () {
         this.emit('NewSession');
     },
     'RouteIntent': function () {
-        this.attributes.route = this.event.request.intent.slots.route.value;
+        var sessionRoute = this.event.request.intent.slots.route.value
+        this.attributes.route = sessionRoute;
         this.handler.state = States.NONE;
 
-        var speechOutput = 'In Ordnung, ich werde nach einer Bahn der Linie ' + this.attributes.route + ' von ' + this.attributes.stop + ' suchen.'
-        this.emit(':tell', speechOutput);
+        var apiRoute = util.getRouteByName(sessionRoute);
+        if (apiRoute === undefined) {
+            this.emit(':tell', 'Entschuldige, diese Linie ist mir leider unbekannt.');
+            return;
+        }
+        this.attributes.apiRoute = apiRoute;
+        var cardTitle = 'KVV - Route';
+        var cardContent = apiRoute.name;
+        this.emit(':tellWithCard', 'Alles klar, du möchtest mit der Linie ' + apiRoute.name + ' fahren.', cardTitle, cardContent);
     },
     'Unhandled': function () {
+        this.emit(':tell', 'Entschuldige, diese Linie kenne ich nicht.');
     },
 });
--- app/handlers/stopHandlers.js
+++ app/handlers/stopHandlers.js
@@ -1,15 +1,25 @@
 const Alexa = require('alexa-sdk');
 const States = require('./states');
+const util = require('../utils/util');
 
 module.exports = Alexa.CreateStateHandler(States.SELECTSTOP, {
     'NewSession': function () {
         this.emit('NewSession');
     },
     'StopIntent': function () {
-        this.attributes.stop = this.event.request.intent.slots.stop.value;
+        var sessionStop = this.event.request.intent.slots.stop.value;
+        this.attributes.sessionStop = sessionStop;
         this.handler.state = States.SELECTROUTE;
 
-        this.emit(':ask', 'Alles klar, du möchtest von der Station ' + this.attributes.stop + ' losfahren. Mit welcher Linie möchtest du fahren?', 'Nenne mir die Linie, mit der du fahren möchtest.');
+        var apiStop = util.getStopByName(sessionStop);
+        if (apiStop === undefined) {
+            this.emit(':tell', 'Entschuldige, diese Station ist mir leider unbekannt.');
+            return;
+        }
+        this.attributes.apiStop = apiStop;
+        var cardTitle = 'KVV - Station';
+        var cardContent = apiStop.name + ' (' + apiStop.id + ')';
+        this.emit(':askWithCard', 'Alles klar, du möchtest von der Station ' + apiStop.name + ' losfahren. Mit welcher Linie möchtest du fahren?', 'Nenne mir die Linie, mit der du fahren möchtest.', cardTitle, cardContent);
     },
     'Unhandled': function () {
     },
