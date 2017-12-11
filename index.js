cbc0db43c2188886c5aedcdbcde12da1c58e76f5
philenius <philippus-perez@web.de>
2017-12-12 00:18:33 +0100

    register handlers for direct departure in index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Alexa = require('alexa-sdk');
 const newSessionHandlers = require('./app/handlers/newSessionHandlers');
 const stopHandlers = require('./app/handlers/stopHandlers');
 const routeHandlers = require('./app/handlers/routeHandlers');
+const directDepartureHandlers = require('./app/handlers/directDepartureHandlers');
 const SpeechOutput = require('./app/resources/SpeechOutput');
 const APP_ID = 'amzn1.ask.skill.df444b69-232f-4e91-bccb-0c5273f3f275';
 
@@ -13,6 +14,6 @@ exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.resources = SpeechOutput;
     alexa.appId = APP_ID;
-    alexa.registerHandlers(newSessionHandlers, stopHandlers, routeHandlers);
+    alexa.registerHandlers(newSessionHandlers, stopHandlers, routeHandlers, directDepartureHandlers);
     alexa.execute();
 };
