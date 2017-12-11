c4ed956eccfeeba1588392449477888acbc1ca70
philenius <philippus-perez@web.de>
2017-12-11 22:40:31 +0100

    delete needless card in LaunchIntent

--- app/handlers/newSessionHandlers.js
+++ app/handlers/newSessionHandlers.js
@@ -11,13 +11,7 @@ module.exports = {
         this.emit('LaunchIntent');
     },
     'LaunchIntent': function () {
-        var cardTitle = 'Willkommen beim KVV';
-        var cardContent = 'VBK - Verkehrsbetriebe Karlsruhe';
-        var imageObj = {
-            smallImageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Tram_at_Karlsruhe_Marktplatz_-_geo.hlipp.de_-_4294.jpg',
-            largeImageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Tram_at_Karlsruhe_Marktplatz_-_geo.hlipp.de_-_4294.jpg'
-        };
-        this.emit(':askWithCard', this.t('WELCOME'), this.t('WELCOME_REPROMPT'), cardTitle, cardContent, imageObj);
+        this.emit(':ask', this.t('WELCOME'), this.t('WELCOME_REPROMPT'));
     },
     'DepartureIntent': function () {
         this.handler.state = States.SELECTSTOP;
