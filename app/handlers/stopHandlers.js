28a60fffa19d351cf573281b60e2c2c1964fbcf4
philenius <philippus-perez@web.de>
2017-12-13 00:03:48 +0100

    fix monster mega bug: cannot use 'this' in callback

--- app/handlers/stopHandlers.js
+++ app/handlers/stopHandlers.js
@@ -14,30 +14,31 @@ module.exports = Alexa.CreateStateHandler(States.SELECTSTOP, {
         var that = this;
         var sessionStop = this.event.request.intent.slots.stop.value;
         this.attributes.data.sessionStop = sessionStop;
-        this.handler.state = States.SELECTROUTE;
 
         var apiStop = util.getStopByName(sessionStop);
-        if (apiStop == undefined) {
+        if (apiStop === undefined) {
             this.emit(':tell', 'Entschuldige, diese Station ist mir leider unbekannt.');
             return;
         }
         this.attributes.data.apiStop = apiStop;
 
+        this.handler.state = States.SELECTROUTE;
+        
         util.getAllDeparturesFromStop(apiStop, function (departures, error) {
             if (error != null) {
-                that.emit(':tell', this.t('UNHANDLED'));
+                that.emit(':tell', that.t('UNHANDLED'));
                 return;
             }
 
             var departingRoutesCount = filter.getCountOfDifferentRoutesDeparting(departures);
             // If there's only one route departing from this station, then don't ask the user for the route. Instead answer directly with the current departures.
             if (departingRoutesCount == 0) {
-                that.emit(':tell', this.t('STOP_INTENT_ANSWER_NO_DEPARTURES', apiStop.name));
+                that.emit(':tell', that.t('STOP_INTENT_ANSWER_NO_DEPARTURES', apiStop.name));
             } else if (departingRoutesCount == 1) {
                 var apiRoute = util.getRouteByName(departures[0].route);
                 // The route may still be unknown because KVV invents weirdo routes sometimes...
                 if (apiRoute == undefined) {
-                    that.emit(':ask', this.t('STOP_INTENT_ANSWER', apiStop.name), this.t('STOP_INTENT_ANSWER_REPROMPT'));
+                    that.emit(':ask', that.t('STOP_INTENT_ANSWER', apiStop.name), that.t('STOP_INTENT_ANSWER_REPROMPT'));
                     return;
                 }
                 util.getNextDeparturesFromStopForRoute(apiStop, apiRoute, function (data, error) {
@@ -46,7 +47,7 @@ module.exports = Alexa.CreateStateHandler(States.SELECTSTOP, {
                     that.emit(':tellWithCard', outputBuilder.buildSpeechOutputForBothDirections(apiStop, apiRoute, relevantDepartures), card[0], card[1]);
                 });
             } else {
-                that.emit(':ask', this.t('STOP_INTENT_ANSWER', apiStop.name), this.t('STOP_INTENT_ANSWER_REPROMPT'));
+                that.emit(':ask', that.t('STOP_INTENT_ANSWER', apiStop.name), that.t('STOP_INTENT_ANSWER_REPROMPT'));
             }
         });
     },
