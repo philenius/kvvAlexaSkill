5e09c7eac2c353257c886cdaba136524b1d2dc46
philenius <philippus-perez@web.de>
2017-12-12 22:31:06 +0100

    activate DynamoDB table

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const newSessionHandlers = require('./app/handlers/newSessionHandlers');
 const stopHandlers = require('./app/handlers/stopHandlers');
 const routeHandlers = require('./app/handlers/routeHandlers');
 const directDepartureHandlers = require('./app/handlers/directDepartureHandlers');
+const standardStopHandlers = require('./app/handlers/standardStopHandlers');
 const SpeechOutput = require('./app/resources/SpeechOutput');
 const APP_ID = 'amzn1.ask.skill.df444b69-232f-4e91-bccb-0c5273f3f275';
 
@@ -14,6 +15,7 @@ exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.resources = SpeechOutput;
     alexa.appId = APP_ID;
-    alexa.registerHandlers(newSessionHandlers, stopHandlers, routeHandlers, directDepartureHandlers);
+    alexa.dynamoDBTableName = 'KvvAlexaSkillUserData';
+    alexa.registerHandlers(newSessionHandlers, stopHandlers, routeHandlers, directDepartureHandlers, standardStopHandlers);
     alexa.execute();
 };
